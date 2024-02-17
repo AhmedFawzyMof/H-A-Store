7569169acf8caefad28fcb1d018dfbf157bcea64
AhmedFawzyMof <np1265448@gmail.com>
2024-02-17 21:09:22 +0200

    H&A:Fix login and register buttons in navbar after login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -179,8 +179,7 @@ export default {
 
         axios.defaults.headers.common["Authorization"] = "Token " + token;
         localStorage.setItem("token", token);
-        const toPath = this.$route.query.to || "/";
-        this.$router.push(toPath);
+        location.reload();
       });
     },
   },
