e47fed8bbae743ad30a4d1c4aee088d0d0d7e540
AhmedFawzyMof <np1265448@gmail.com>
2024-03-20 07:45:37 +0200

    Add: slide images and categories

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
-  <carousel :items-to-show="1" class="mt-5 mb-5">
-    <slide v-for="product in latestProducts" :key="product.id">
+  <carousel
+    :items-to-show="1"
+    :autoplay="5000"
+    :pauseAutoplayOnHover="true"
+    :touchDrag="true"
+    :wrapAround="true"
+    class="mt-5 mb-5"
+  >
+    <slide v-for="(slide, index) in slides" :key="index">
       <router-link
-        :to="'/products/' + product.slug"
+        :to="'/products/' + slide.product"
         class="w-[80%] h-52 bg-slate-500 flex items-center justify-center rounded-lg"
       >
-        <h1 class="text-4xl text-white">Offers Up To 50%</h1>
+        <img :src="slide.img" class="h-full rounded-lg w-full" />
       </router-link>
     </slide>
 
@@ -14,7 +21,14 @@
       <pagination />
     </template>
   </carousel>
-  <div id="product-collection">
+  <div class="categories p-3 flex gap-5 items-center justify-center flex-wrap">
+    <img
+      v-for="category in categories"
+      :src="category.img"
+      class="w-1/4 h-[84px] flex items-center justify-center text-white rounded-full"
+    />
+  </div>
+  <!-- <div id="product-collection">
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
       <ul class="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <ProductCard
@@ -24,7 +38,7 @@
         />
       </ul>
     </div>
-  </div>
+  </div> -->
 </template>
 <script>
 import axios from "axios";
@@ -42,7 +56,13 @@ export default {
   },
   data() {
     return {
+      slides: [
+        { product: "Fitness-Whatchamacallit", img: "/img/slide/1.jpg" },
+        { product: "Kitchen-Gear", img: "/img/slide/1.jpg" },
+        { product: "Dog-Tool", img: "/img/slide/1.jpg" },
+      ],
       latestProducts: [],
+      categories: [],
     };
   },
   mounted() {
@@ -51,14 +71,17 @@ export default {
   },
   methods: {
     async getLatestProducts() {
-
-      this.$store.state.loading = true
+      this.$store.state.loading = true;
       await axios.get("/allproducts").then((response) => {
         const Products = response.data.Products;
         this.latestProducts = Products;
       });
-      
-      this.$store.state.loading = false
+      await axios.get("/allcategories").then((response) => {
+        const Categories = response.data.Categories;
+        this.categories = Categories;
+      });
+
+      this.$store.state.loading = false;
     },
   },
 };
