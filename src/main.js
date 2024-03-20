eb38579d0bf150febfa96c904dad8f0c9fdd7e9f
AhmedFawzyMof <np1265448@gmail.com>
2024-03-20 06:27:59 +0200

    Add: logo image

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from "./router/index";
 import store from "./store/index";
 import axios from "axios";
 import "./style.css";
-axios.defaults.baseURL = "https://h-a-stroe-backend.onrender.com";
+axios.defaults.baseURL = "http://localhost:5500";
 
 createApp(App).use(store).use(router, axios).mount("#app");
